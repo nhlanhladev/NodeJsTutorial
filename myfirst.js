54fd830a30bd7e9b1325d76755d9480c9eb0263a
nhlanhlaDEV <dhlamininhlanhla1@gmail.com>
2022-08-03 06:36:22 +0200

    added new http request

--- myfirst.js
+++ myfirst.js
@@ -7,3 +7,10 @@ http
     res.end("Hello World!");
   })
   .listen(8080);
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Hello World!");
+  })
+  .listen(8080);
